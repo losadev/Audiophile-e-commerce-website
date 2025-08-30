6c09a253c8a58b309675b9255eb8a1927f7eccca
losadev <losadauresdev@gmail.com>
2025-08-30 15:38:42 +0200

    see product card

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import CategoryCard from "../components/CategoryCard";
 
 function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="h-full">
       <section
         className="relative min-h-[560px] bg-[url('/assets/home/mobile/image-header.jpg')] bg-cover bg-no-repeat bg-bottom text-white
 "
@@ -19,24 +20,63 @@ function Home() {
           <button className="btn-primary sub-title">See product</button>
         </div>
       </section>
+      <div className="p-8 space-y-32">
+        <section className="flex flex-col gap-16 ">
+          <CategoryCard
+            alt="headphones"
+            src="/assets/home/mobile/image-removebg-headphones.png"
+            title="Headphones"
+          />
+          <CategoryCard
+            alt="speakers"
+            src="/assets/home/mobile/image-removebg-speakers.png"
+            title="Headphones"
+          />
+          <CategoryCard
+            alt="earphones"
+            src="/assets/home/mobile/image-removebg-earphones.png"
+            title="Earphones"
+          />
+        </section>
 
-      <section className="p-8 flex flex-col gap-16 ">
-        <CategoryCard
-          alt="headphones"
-          src="/assets/home/mobile/image-removebg-headphones.png"
-          title="Headphones"
-        />
-        <CategoryCard
-          alt="speakers"
-          src="/assets/home/mobile/image-removebg-speakers.png"
-          title="Headphones"
-        />
-        <CategoryCard
-          alt="earphones"
-          src="/assets/home/mobile/image-removebg-earphones.png"
-          title="Earphones"
-        />
-      </section>
+        <section className="bg-[var(--color-pink)] flex flex-col items-center rounded-lg relative py-8">
+          {/*ANILLO EXTERIOR*/}
+          <div className="border-[var(--color-white-secondary)] rounded-full p-4 border w-full absolute top-0 ">
+            {/*ANILLOS INTERIOR*/}
+            <div className=" border-[var(--color-white-secondary)] border p-8 rounded-full w-full">
+              <Image
+                src="/assets/home/mobile/image-speaker-zx9.png"
+                alt="speaker"
+                width={172}
+                height={150}
+                className="mx-auto"
+              />
+            </div>
+          </div>
+          <div className="mt-65 flex flex-col gap-4 items-center">
+            <h1 className="h2 text-[var(--color-white)] text-center px-8">
+              ZX9 SPEAKER
+            </h1>
+            <p className="text-center text-[var(--color-white-secondary)] text-[15px]">
+              Upgrade to premium speakers that are phenomenally built to deliver
+              truly remarkable sound.
+            </p>
+            <button className="btn-secondary bg-[var(--color-black-secondary)] text-[var(--color-white-secondary)]">
+              SEE PRODUCT
+            </button>
+          </div>
+        </section>
+        <section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            alias, nemo nostrum, laborum iure ratione, sit soluta culpa quasi
+            provident velit unde repellat quos? Sed id nihil suscipit vel
+            repudiandae?
+          </p>
+        </section>
+
+        <section className="rounded-lg"></section>
+      </div>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,11 @@
-import ButtonShop from "./components/ButtonShop";
-import CustomInput from "./components/CustomInput";
 import NavBar from "./components/NavBar";
-import NumbersButton from "./components/NumbersButton";
 import Home from "./home/page";
 
 export default function Page() {
   return (
-    <div className="">
+    <div className="h-full">
       <NavBar algo="" />
       <Home />
-      <h1 className="h1 text-[var(--color-pink)]">HOLA MUNDO</h1>
-      <h2 className="h2">H2</h2>
-      <h4 className="h4">h4</h4>
-      <h5 className="h5">h5</h5>
-      <h6 className="h6">h6</h6>
-      <p className="overline-app">Overline</p>
-      <p className="sub-title">Esto es un subtitulo</p>
-      <button className="btn-primary sub-title">BTN PRIMARY</button>
-      <button className="btn-secondary sub-title">BTN PRIMARY</button>
-      <ButtonShop />
-      <CustomInput
-        type="text"
-        label="Name"
-        name="name"
-        placeholder="Insert your name here"
-      />
-      <CustomInput type="radio" name="payment" />
-      <NumbersButton />
     </div>
   );
 }
